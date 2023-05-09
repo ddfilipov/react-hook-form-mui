ea6e3712544bce52ecc13c08c4d9622e81e14acd
denis.filipov <denisfilipov12+3@gmail.com>
2023-05-09 20:19:18 +0200

    added onchange that doesnt work

--- src/components/CustomInputText.tsx
+++ src/components/CustomInputText.tsx
@@ -1,14 +1,15 @@
-import { FC } from "react";
+import { ChangeEventHandler, FC } from "react";
 
 interface CustomInputTextProps {
     label: string;
 }
 
 export const CustomInputText: FC<CustomInputTextProps> = ({ label }) => {
+    const handleChangeValue = (e:  ChangeEventHandler<HTMLInputElement>) => {};
     return (
         <>
             <label>{label}</label>
-            <input type="text" />
+            <input type="text" onChange={(e: ChangeEventHandler<HTMLInputElement>) => handleChangeValue(e)} />
         </>
     );
 };
