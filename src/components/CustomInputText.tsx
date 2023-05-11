a831d83c4b9f6427e72f3b7c889ea3e0e85263ba
denis.filipov <denisfilipov12+3@gmail.com>
2023-05-11 18:49:02 +0200

    styling custom text input

--- src/components/CustomInputText.tsx
+++ src/components/CustomInputText.tsx
@@ -1,23 +1,29 @@
+import styled from "@emotion/styled";
 import { ChangeEvent, ChangeEventHandler, FC, useState } from "react";
 
 interface CustomInputTextProps {
     label: string;
 }
 
+const StyledInput = styled.div`
+    max-width: 300px;
+    display: grid;
+    grid-template-rows: 1fr 1fr;
+`;
 export const CustomInputText: FC<CustomInputTextProps> = ({ label }) => {
     const [inputValue, setInputValue] = useState<string>("");
     const handleChangeValue = (val: string) => {
         setInputValue(val);
     };
-    console.log(inputValue)
+    console.log(inputValue);
     return (
-        <>
+        <StyledInput>
             <label>{label}</label>
             <input
                 type="text"
                 onChange={(e: ChangeEvent<HTMLInputElement>) => handleChangeValue(e.target.value)}
                 value={inputValue}
             />
-        </>
+        </StyledInput>
     );
 };
