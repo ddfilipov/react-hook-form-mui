6d0eae93ad7bd7db7db5676d7fbe1db8b08f1887
denis.filipov <denisfilipov12+3@gmail.com>
2023-05-23 09:22:13 +0200

    more styles

--- src/themes/compStyleOverride.tsx
+++ src/themes/compStyleOverride.tsx
@@ -3,17 +3,49 @@ import { createTheme } from "@mui/material";
 export default function componentStyleOverrides(theme: any) {
     return createTheme({
         components: {
+            MuiFormLabel: {
+                styleOverrides: {
+                    root: {
+                        "&.MuiFormLabel-root": {
+                            color: "white",
+                        },
+                    },
+                },
+            },
+            MuiInputBase: {
+                styleOverrides: {
+                    root: {
+                        "&.MuiInputBase-root:before": {
+                            borderBottom: "none",
+                            content: "none",
+                        },
+                        "&.MuiInputBase-root:after": {
+                            borderBottom: "none",
+                        },
+                        "&.MuiInputBase-root": {
+                            border: `1px solid white`,
+                            color: "white",
+                        },
+                        "&.css-1lkr5rz-MuiInputBase-input-MuiOutlinedInput-input": {
+                            color: "white",
+                        },
+                    },
+                },
+            },
+            MuiSvgIcon: {
+                styleOverrides: {
+                    root: {
+                        "&.MuiSvgIcon-root": {
+                            color: "white",
+                        },
+                    },
+                },
+            },
             MuiSelect: {
                 styleOverrides: {
                     select: {
                         color: "white",
-                    },
-                },
-            },
-            MuiAutocomplete: {
-                styleOverrides: {
-                    listbox: {
-                        fontFamily: "Roobert",
+                        borderColor: "white",
                     },
                 },
             },
